8f5cd7e643568bb583964f4906f85a4ddceb873c
buhmann <buhmann81@gmail.com>
2026-06-18 17:14:26 +0200

    Initial commit

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Buhmann_StockStatusSmile', __DIR__);
